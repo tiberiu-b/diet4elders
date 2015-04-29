35ad130d80f151d9c218a1c15bf5cc400207d8fc
cristiprg <cristian.prigoana@yahoo.com>
2015-04-29 08:50:06 +0300

    Integrate HBMO from MainAlgorithm.

--- src/main/java/org/licenta/d4elders/main/Diet4Elders.java
+++ src/main/java/org/licenta/d4elders/main/Diet4Elders.java
@@ -1,5 +1,8 @@
 package org.licenta.d4elders.main;
 
+import org.licenta.d4elders.algorithm.HoneyBeeMatingOptimiziation;
+import org.licenta.d4elders.algorithm.MainAlgorithm.RunInformation;
+import org.licenta.d4elders.model.Solution;
 import org.licenta.d4elders.model.user_profile.NutritionalRecommandationHelper;
 import org.licenta.d4elders.model.user_profile.UserProfileStub;
 
@@ -12,8 +15,21 @@ public class Diet4Elders {
 	}
 
 	public void run() {
-		AlgorithmVer1 a1 = new AlgorithmVer1();
-		a1.start();
+		//AlgorithmVer1 a1 = new AlgorithmVer1();
+		//a1.start();
+
+
+		HoneyBeeMatingOptimiziation HBMO = new HoneyBeeMatingOptimiziation();
+        Solution queen = HBMO.performAlgorithm();
+        RunInformation info = HBMO.getLastRunInformation();
+
+        // Print results
+        System.out.println("___\nFinal Result:");
+        System.out.println(queen + " Fitness: " + queen.getFitness());
+        System.out.println("Number of iterations: " + info.nrOfItertions);
+
+        System.out.println("Duration of execution(in millis): " + info.duration);
+
 	}
 
 }
