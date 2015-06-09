f9c83b21165171227fa4abe78ebc2e9336c08fdc
cristiprg <cristian.prigoana@yahoo.com>
2015-06-10 00:43:00 +0300

    Add support for multiple config sections - very useful for testing certain parameters without much overhead

--- src/main/java/d4elders/algorithm/helper/ConfigurationsGenerator.java
+++ src/main/java/d4elders/algorithm/helper/ConfigurationsGenerator.java
@@ -39,92 +39,99 @@ public class ConfigurationsGenerator {
 	 * @return an ArrayList<AlgorithmConfiguration> that contains all the possible combinations.
 	 */
 	public static ArrayList<AlgorithmConfiguration> getConfigFromIniFile(String iniFilePath){
-		AlgorithmConfigurationArrays config = new AlgorithmConfigurationArrays();
+		ArrayList<AlgorithmConfigurationArrays> configPacks = new ArrayList<AlgorithmConfigurationArrays>();
 		// ini examples here: http://ini4j.sourceforge.net/tutorial/OneMinuteTutorial.java.html
 		try {
 			Wini ini = new Wini(new File(iniFilePath));
-			Section section = ini.get("config");
-			for(String optionKey : section.keySet()){
-				String[] values = section.get(optionKey).split(",");
-				switch(optionKey){
-				case "BroodModificationStrategy":
-					for(String value : values)
-						config.addBroodModificationStrategy(value);
-					break;
-				case "BroodImprovementStrategies":
-					for(String value : values)
-					{
-						ArrayList<String> strategies = new ArrayList<String>();
-						for(String strategy : value.split(";"))
+
+			for(String sectionName : ini.keySet()){
+				AlgorithmConfigurationArrays config = new AlgorithmConfigurationArrays(); // un pack
+				Section section  = ini.get(sectionName);
+				for(String optionKey : section.keySet()){
+					String[] values = section.get(optionKey).split(",");
+					switch(optionKey){
+					case "fileName":
+						break;
+					case "BroodModificationStrategy":
+						for(String value : values)
+							config.addBroodModificationStrategy(value);
+						break;
+					case "BroodImprovementStrategies":
+						for(String value : values)
 						{
-							strategies.add(strategy.trim());
+							ArrayList<String> strategies = new ArrayList<String>();
+							for(String strategy : value.split(";"))
+							{
+								strategies.add(strategy.trim());
+							}
+							config.addWorkerModificationStrategies(strategies);
 						}
-						config.addWorkerModificationStrategies(strategies);
+						break;
+					case "PopSize":
+						for(String value : values)
+							config.addPopSize(Integer.valueOf(value));
+						break;
+					case "MaxNrMatings":
+						for(String value : values)
+							config.addMaxNrMatings(Integer.valueOf(value));
+						break;
+					case "InitialSpeed":
+						for(String value : values)
+							config.addInitialSpeed(Integer.valueOf(value));
+						break;
+					case "initialEnergy":
+						for(String value : values)
+							config.addInitialSpeed(Integer.valueOf(value));
+						break;
+					case "speedReductionFactor":
+						for(String value : values)
+							config.addSpeedReductionFactor(Double.valueOf(value));
+						break;
+					case "energyReductionAmount":
+						for(String value : values)
+							config.addEnergyReductionAmlount(Double.valueOf(value));
+						break;
+					case "probabilityToMateDroneThreshold":
+						for(String value : values)
+							config.addProbabilityToMateDroneThreshold(Double.valueOf(value));
+						break;
+					case "similarityCoefficientThreshold":
+						for(String value : values)
+							config.addSimilarityCoefficientThreshold(Double.valueOf(value));
+						break;
+					case "hillClimbNeighSize":
+						for(String value : values)
+							config.addHillClimbingNeighborhoodSize(Integer.valueOf(value));
+						break;
+					case "T0":
+						for(String value : values)
+							config.addT0(Double.valueOf(value));
+						break;
+					case "alpha":
+						for(String value : values)
+							config.addAlpha(Double.valueOf(value));
+						break;
+					case "Tmin":
+						for(String value : values)
+							config.addTmin(Double.valueOf(value));
+						break;
+					case "tabuSearchIters":
+						for(String value : values)
+							config.addMaxNrIterations(Integer.valueOf(value));
+						break;
+					case "tabuSize":
+						for(String value : values)
+							config.addTabuSize(Integer.valueOf(value));
+						break;
+					case "tabuNeighSize":
+						for(String value : values)
+							config.addTabuNeighSize(Integer.valueOf(value));
+						break;
+					default:
+						log.log(Level.WARNING, "Unkown key " + optionKey + " in the ini configuration file.");
 					}
-					break;
-				case "PopSize":
-					for(String value : values)
-						config.addPopSize(Integer.valueOf(value));
-					break;
-				case "MaxNrMatings":
-					for(String value : values)
-						config.addMaxNrMatings(Integer.valueOf(value));
-					break;
-				case "InitialSpeed":
-					for(String value : values)
-						config.addInitialSpeed(Integer.valueOf(value));
-					break;
-				case "initialEnergy":
-					for(String value : values)
-						config.addInitialSpeed(Integer.valueOf(value));
-					break;
-				case "speedReductionFactor":
-					for(String value : values)
-						config.addSpeedReductionFactor(Double.valueOf(value));
-					break;
-				case "energyReductionAmount":
-					for(String value : values)
-						config.addEnergyReductionAmlount(Double.valueOf(value));
-					break;
-				case "probabilityToMateDroneThreshold":
-					for(String value : values)
-						config.addProbabilityToMateDroneThreshold(Double.valueOf(value));
-					break;
-				case "similarityCoefficientThreshold":
-					for(String value : values)
-						config.addSimilarityCoefficientThreshold(Double.valueOf(value));
-					break;
-				case "hillClimbNeighSize":
-					for(String value : values)
-						config.addHillClimbingNeighborhoodSize(Integer.valueOf(value));
-					break;
-				case "T0":
-					for(String value : values)
-						config.addT0(Double.valueOf(value));
-					break;
-				case "alpha":
-					for(String value : values)
-						config.addAlpha(Double.valueOf(value));
-					break;
-				case "Tmin":
-					for(String value : values)
-						config.addTmin(Double.valueOf(value));
-					break;
-				case "tabuSearchIters":
-					for(String value : values)
-						config.addMaxNrIterations(Integer.valueOf(value));
-					break;
-				case "tabuSize":
-					for(String value : values)
-						config.addTabuSize(Integer.valueOf(value));
-					break;
-				case "tabuNeighSize":
-					for(String value : values)
-						config.addTabuNeighSize(Integer.valueOf(value));
-					break;
-				default:
-					log.log(Level.WARNING, "Unkown key " + optionKey + " in the ini configuration file.");
 				}
+				configPacks.add(config);
 			}
 
 		} catch (InvalidFileFormatException e) {
@@ -133,7 +140,10 @@ public class ConfigurationsGenerator {
 			e.printStackTrace();
 		}
 
+		ArrayList<AlgorithmConfiguration> configs = new ArrayList<AlgorithmConfiguration>();
+		for(AlgorithmConfigurationArrays config : configPacks)
+			configs.addAll(config.getAllConfigrations());
 
-		return config.getAllConfigrations();
+		return configs;
 	}
 }
